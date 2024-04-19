f228f69fe7e7620cb40db3e9aa9290d01ceb9c01
Ghazimoradi <ghazimoradi.amin@gmail.com>
2024-04-19 14:42:46 +0330

    implementation Touch Response

--- app/src/main/java/project/GameView.java
+++ app/src/main/java/project/GameView.java
@@ -4,17 +4,23 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class GameView extends View {
 
     private Paint dotPaint;
+    private Paint touchPaint;
     private Paint textPaint;
     private Paint linePaint;
     private int cols = 4;
@@ -24,13 +30,49 @@ public class GameView extends View {
     private int space = 150;
     private int radius = 15;
 
-    private static class Point {
+    private float touchX;
+    private float touchY;
+
+    private boolean isDebugMode = false;
+    private boolean isSide1 = true;
+
+    private ArrayList<Line> lines = new ArrayList<>();
+
+    private static class Position {
         public int x;
         public int y;
 
-        public Point(int x, int y) {
+        public Position(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+    }
+
+    private static class Diff {
+        public int i;
+        public int j;
+        public Float diff;
+
+        public Diff(int i, int j, float diff) {
+            this.i = i;
+            this.j = j;
+            this.diff = diff;
+        }
+    }
+
+    private static class Line {
+        public int i1;
+        public int j1;
+        public int i2;
+        public int j2;
+        public int playerIndex;
+
+        public Line(int i1, int j1, int i2, int j2, int playerIndex) {
+            this.i1 = i1;
+            this.j1 = j1;
+            this.i2 = i2;
+            this.j2 = j2;
+            this.playerIndex = playerIndex;
         }
     }
 
@@ -55,6 +97,11 @@ public class GameView extends View {
         dotPaint.setColor(Color.WHITE);
         dotPaint.setStyle(Paint.Style.FILL);
         dotPaint.setAntiAlias(true);
+
+        touchPaint = new Paint();
+        touchPaint.setColor(Color.RED);
+        touchPaint.setStyle(Paint.Style.FILL);
+        touchPaint.setAntiAlias(true);
 
         linePaint = new Paint();
         linePaint.setColor(Color.parseColor("#4444ff"));
@@ -83,42 +130,93 @@ public class GameView extends View {
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
+
         canvas.drawColor(Color.parseColor("#222222"));
-        connect(canvas, 0, 2, 1, 2);
-        connect(canvas, 1, 3, 1, 2);
+
+        for (Line line :
+                lines) {
+            connect(canvas, line);
+        }
         for (int i = 0; i < cols; i++) {
             for (int j = 0; j < rows; j++) {
-                Point point = computePoint(i, j);
+                Position point = computePoint(i, j);
                 canvas.drawCircle(point.x, point.y, radius, dotPaint);
             }
         }
-        debugNaming(canvas);
+
+        if (isDebugMode) {
+            canvas.drawCircle(touchX, touchY, 10, touchPaint);
+            debugNaming(canvas);
+        }
     }
 
-    private boolean connect(Canvas canvas, int i1, int j1, int i2, int j2) {
-        float c = (float) Math.sqrt(Math.pow(i1 - i2, 2) + Math.pow(j1 - j2, 2));
-        if (c != 1) {
-            return false;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        touchX = event.getX();
+        touchY = event.getY();
+
+        detectConnectingLine();
+        invalidate();
+        isSide1 = !isSide1;
+        return super.onTouchEvent(event);
+    }
+
+    public void detectConnectingLine() {
+        ArrayList<Diff> diffs = new ArrayList<>();
+
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                Position position = computePoint(i, j);
+                float diff = computeDiff(touchX, touchY, position.x, position.y);
+                diffs.add(new Diff(i, j, diff));
+            }
         }
-        Point p1 = computePoint(i1, j1);
-        Point p2 = computePoint(i2, j2);
+        Collections.sort(diffs, new Comparator<Diff>() {
+            @Override
+            public int compare(Diff o1, Diff o2) {
+                return o1.diff.compareTo(o2.diff);
+            }
+        });
+        Diff min1 = diffs.get(0);
+        Diff min2 = diffs.get(1);
+
+        if (min1.diff > space / 2) {
+            return;
+        }
+
+        lines.add(new Line(min1.i, min1.j, min2.i, min2.j, isSide1 ? 1 : 2));
+        Log.i("LOG", "" + min1.i + ", " + min1.j);
+        Log.i("LOG", "" + min2.i + ", " + min2.j);
+    }
+
+    private float computeDiff(float x1, float y1, float x2, float y2) {
+        return (float) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+    }
+
+    private void connect(Canvas canvas, Line line) {
+        Position p1 = computePoint(line.i1,line.j1);
+        Position p2 = computePoint(line.i2, line.j2);
+        if (line.playerIndex == 1) {
+            linePaint.setColor(Color.parseColor("#4444ff"));
+        } else {
+            linePaint.setColor(Color.parseColor("#ff4444"));
+        }
         canvas.drawLine(p1.x, p1.y, p2.x, p2.y, linePaint);
-        return true;
     }
 
     private void debugNaming(Canvas canvas) {
         for (int i = 0; i < cols; i++) {
             for (int j = 0; j < rows; j++) {
                 String name = "" + i + "," + j;
-                Point point = computePoint(i, j);
+                Position point = computePoint(i, j);
                 canvas.drawText(name, point.x, point.y + 50, textPaint);
             }
         }
     }
 
-    private Point computePoint(int i, int j) {
+    private Position computePoint(int i, int j) {
         int x = offsetX + (i * space);
         int y = offsetY + ((rows - 1 - j) * space);
-        return new Point(x, y);
+        return new Position(x, y);
     }
 }
